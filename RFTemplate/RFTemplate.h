a62a78cf3b58c625b602c95a05899d39df6c5a16
Brandon Sneed <brandon@redf.net>
2016-10-30 16:19:16 -0700

    Initial Commit

---
+++ RFTemplate/RFTemplate.h
@@ -0,0 +1,19 @@
+//
+//  RFTemplate.h
+//  RFTemplate
+//
+//  Created by Sneed, Brandon on 10/30/16.
+//  Copyright © 2016 Redf. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RFTemplate.
+FOUNDATION_EXPORT double RFTemplateVersionNumber;
+
+//! Project version string for RFTemplate.
+FOUNDATION_EXPORT const unsigned char RFTemplateVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RFTemplate/PublicHeader.h>
+
+
